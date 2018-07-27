8ea77326bad0116fd6b754ac128b58e348428bdd
Mahmoud Mohammed <progmahmoud95@gmail.com>
2018-07-27 01:02:12 -0700

    Registration view

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by mahmoud on 7/27/18.
+//  Copyright © 2018 mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+   
+
+}
