7231941e80c7f883e7356e8b3c1e6b9cd40fda06
Mahmoud Mohammed <mahmoud1@192.168.204.128>
2018-08-01 07:26:23 -0700

    authServices added

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = userNameTxt.text , userNameTxt.text != "",
+              let email = emailTxt.text , emailTxt.text != "",
+              let pass = passTxt.text , passTxt.text != "" else { return }
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name,UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
+                    }
+                })
+            }
+        }
         
     }
     
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
     
     @IBAction func closedPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
+    
+    	
+    
     
 
 }
