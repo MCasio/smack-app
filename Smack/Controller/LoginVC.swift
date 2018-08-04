a98487b69d2f528080c438c10a037d9d03154a0e
Mahmoud Mohammed <mahmoud1@192.168.204.128>
2018-08-04 00:58:08 -0700

    login user functionality added

--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -10,12 +10,38 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    // Outlets
+    
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var passWordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
+        
     }
 
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = userNameTxt.text , userNameTxt.text != "",
+              let pass = passWordTxt.text , passWordTxt.text != "" else { return }
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                    self.spinner.isHidden = true
+                    self.spinner.stopAnimating()
+                    self.dismiss(animated: true, completion: nil)
+                })
+            }
+        }
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
@@ -23,6 +49,19 @@ class LoginVC: UIViewController {
     @IBAction func createAccountBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
+    
+    func setupView() {
+        spinner.isHidden = true
+        userNameTxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor : SMACK_PURPLE_PLACEHOLDER])
+        passWordTxt.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedStringKey.foregroundColor : SMACK_PURPLE_PLACEHOLDER])
+    }
+    
+    
+    
+    
+    
+    
+    
     
 
 }
