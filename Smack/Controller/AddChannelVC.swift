8e00a42704a7972f8bb39fd444570a98e540f94a
Mahmoud Mohammed <mahmoud1@192.168.204.128>
2018-08-04 04:29:17 -0700

    add channel view added

---
+++ Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,45 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Mahmoud Mohammed on 8/4/18.
+//  Copyright © 2018 mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+
+        
+    }
+
+    func setupView() {
+        let closeTap = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTap)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: SMACK_PURPLE_PLACEHOLDER])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: SMACK_PURPLE_PLACEHOLDER])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeModelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+    
+
+}
